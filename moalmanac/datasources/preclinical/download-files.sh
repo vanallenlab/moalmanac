9fbda13404cd5b6078ad2d66d025d87823fb1df4
Brendan Reardon <breardon@broadinstitute.org>
2022-02-04 13:29:06 -0500

    Added file sizes and md5 to preclinical download

--- moalmanac/datasources/preclinical/download-files.sh
+++ moalmanac/datasources/preclinical/download-files.sh
@@ -6,3 +6,5 @@ gdown https://drive.google.com/uc?id=13aI3_9CozP7t1ThcPBZfBpuQLWPBBt3X
 gdown https://drive.google.com/uc?id=17KsBFsMAV6oda5Bk_AaDeowQcFiXcpV5
 gdown https://drive.google.com/uc?id=1-Cfd8LMgfm_3veTFkYo2174Y2QGH8Ac7
 gdown https://drive.google.com/uc?id=15ShnYYD9OKbQ4MmSywTqDFYEISraEohl
+
+md5 -r ./*.evaluated.txt
