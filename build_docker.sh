51eea918da94f3919b6b6c7c8de6fe053d23c05a
Brendan Reardon <breardon@broadinstitute.org>
2025-01-27 15:23:25 -0500

    Updated docker build to support multiple architectures

--- build_docker.sh
+++ build_docker.sh
@@ -6,11 +6,8 @@ sed -i "" -e "s#colnames.ini#/moalmanac/colnames.ini#" moalmanac/config.py
 sed -i "" -e "s#../datasources#/datasources#" moalmanac/annotation-databases.ini
 sed -i "" -e "s#../datasources#/datasources#" moalmanac/preclinical-databases.ini
 
-docker build -t vanallenlab/moalmanac:"${tag}" .
-docker push vanallenlab/moalmanac:"${tag}"
-
-docker build -t vanallenlab/moalmanac:latest .
-docker push vanallenlab/moalmanac:latest
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t vanallenlab/moalmanac:"${tag}" --push .
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t vanallenlab/moalmanac:latest --push .
 
 sed -i "" -e "s#/moalmanac/colnames.ini#colnames.ini#" moalmanac/config.py
 sed -i "" -e "s#/datasources#../datasources#" moalmanac/annotation-databases.ini
